bbcd2a4b7a3c11bc84cadad3af787fe6bc65b695
Jeffrey Lee <jlee@spokeo.com>
2016-04-26 14:37:18 -0700

    add log

--- samples/basic_sample/consumer/sample_kcl_app.js
+++ samples/basic_sample/consumer/sample_kcl_app.js
@@ -51,12 +51,9 @@ function recordProcessor() {
       var record, data, sequenceNumber, partitionKey;
       for (var i = 0 ; i < records.length ; ++i) {
         record = records[i];
-        data = new Buffer(record.data, 'base64').toString();
+        data = new Buffer(record.data, 'base64');
         log.info("====================data================");
-        log.info(data);
         log.info(typeof(data));
-        log.info(data["time"]);
-        log.info(data["reading"]);
         sequenceNumber = record.sequenceNumber;
         partitionKey = record.partitionKey;
         log.info(util.format('ShardID: %s, Record: %s, SeqenceNumber: %s, PartitionKey:%s', shardId, data, sequenceNumber, partitionKey));
