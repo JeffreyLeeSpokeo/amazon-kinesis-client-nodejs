13f8c748f51b37e78d6694de8bab121908e0b6e6
Jeffrey Lee <jlee@spokeo.com>
2016-04-26 18:36:22 -0700

    add modified redis code

--- samples/basic_sample/consumer/sample_kcl_app.js
+++ samples/basic_sample/consumer/sample_kcl_app.js
@@ -69,7 +69,7 @@ function recordProcessor() {
         log.info(redis_data);
         redisClient.set("123", redis_data);
         var test = redisClient.get("123", function(err, reply) {
-            log.info(reply.toString());
+            log.info(reply);
           }
         );
         log.info("=====================redis==================");
